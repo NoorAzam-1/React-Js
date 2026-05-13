b75d482853ce3aab8db61f17c680aa42ac5990e2
NoorAzam-1 <azamnoor698@gmail.com>
2026-05-13 15:28:25 +0530

    chore: add blank line for better readability in UseEffect component

--- react/src/hooks/UseEffect.jsx
+++ react/src/hooks/UseEffect.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+
 // Example:
 // API Calling, Login, Data Fetching, Timer, Local Storage, 
 
