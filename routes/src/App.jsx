fe766cf03f9a17742ef10b4fa36a094f3928ef4b
NoorAzam-1 <180100808+NoorAzam-1@users.noreply.github.com>
2026-09-19 18:58:18 +0530

    Add Event route to App component

--- routes/src/App.jsx
+++ routes/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Event from "./pages/Event";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/events" element={<Event />} />
       </Routes>
     </>
   );
